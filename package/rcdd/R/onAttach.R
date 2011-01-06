8a8fba584d7b78bf68ccabb54c6834902bf7d37a
Charles J. Geyer <charlie@stat.umn.edu>
2011-01-06 07:10:13 -0600

    Version 1.1-4 ready for CRAN

---
+++ package/rcdd/R/onAttach.R
@@ -0,0 +1,7 @@
+
+.onAttach <- function(lib, pkg) {
+     cat("If you want correct answers, use rational arithmetic.\n")
+     cat("See the Warnings sections added to help pages for\n")
+     cat("    functions that do computational geometry.\n")
+}
+
