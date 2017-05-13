71d58c92340b45375d5623f09f24188917db7b4a
Charles J. Geyer <charlie@stat.umn.edu>
2017-05-12 20:28:19 -0500

    This was version 1.1-13 to CRAN

---
+++ devel/valgrind/scdd.R
@@ -0,0 +1,20 @@
+
+ set.seed(42)
+
+ library("rcdd")
+
+ d <- 15
+ ng <- 25 
+ x <- rnorm(d * ng)
+ x <- matrix(x, ncol = d)
+ vrep <- cbind(as.numeric(runif(ng) < 0.5), x)
+ vrep <- cbind(as.numeric(runif(ng) < 0.5), vrep)
+ vrep <- d2q(vrep)
+
+ hout <- scdd(vrep, representation = "V")
+ vout <- scdd(hout$output)
+ hout <- scdd(vout$output, adjacency = TRUE)
+ vout <- scdd(hout$output, inputadjacency = TRUE)
+ hout <- scdd(vout$output, inputincidence = TRUE)
+ vout <- scdd(hout$output, incidence = TRUE)
+
