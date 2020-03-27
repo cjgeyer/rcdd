9f7b4c4872cf7a61049e63239b8900a6a9457ad8
Charles J. Geyer <charlie@stat.umn.edu>
2020-03-26 19:47:13 -0500

    Looks like this was ready to ship
    But wasn't.  Don't remember why.

--- package/rcdd/R/onAttach.R
+++ package/rcdd/R/onAttach.R
@@ -2,7 +2,7 @@
 .onAttach <- function(lib, pkg) {
      packageStartupMessage(
          "If you want correct answers, use rational arithmetic.\n",
-         "See the Warnings sections added to help pages for\n",
-         "    functions that do computational geometry.\n")
+         "See the Warnings sections in help pages for\n",
+         "    functions that do computational geometry.")
 }
 
